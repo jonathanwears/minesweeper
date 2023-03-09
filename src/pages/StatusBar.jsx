64babda120d35c53a81fc268917cb582aa82d9b7
Jonathan <jonathanwears@outlook.com>
2023-03-09 16:57:24 +0100

    Create StatusBar.jsx

---
+++ src/pages/StatusBar.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import useGameStore from '../utils/stores/useGameStore';
+import './status-bar.css';
+import StatusButtons from './StatusButtons';
+import StatusMessage from './StatusMessage';
+
+function StatusBar() {
+  const { isWon, isLost } = useGameStore((state) => state.game);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (isWon) {
+      setMessage('You Won!');
+    } else if (isLost) {
+      setMessage('aww You Lost!');
+    } else {
+      setMessage('');
+    }
+  }, [isLost, isWon]);
+
+  return (
+    <div className="status-bar">
+      <StatusButtons />
+      <StatusMessage message={message} />
+    </div>
+  );
+}
+
+export default StatusBar;
