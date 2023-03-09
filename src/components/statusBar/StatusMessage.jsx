a9bd7e1c186c8e9c894b70c13d38700853dd6364
Jonathan <jonathanwears@outlook.com>
2023-03-09 16:57:20 +0100

    Create StatusMessage.jsx

---
+++ src/components/statusBar/StatusMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StatusMessage({ message }) {
+  return (
+    <p className="statusMessage">
+      {message}
+    </p>
+  );
+}
+
+export default StatusMessage;
