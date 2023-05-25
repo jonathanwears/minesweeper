d33022d57a4efe290f32288049a5798874d7e783
Jonathan <jonathanwears@outlook.com>
2023-05-25 16:22:28 +0200

    renamed test

--- e2e/test.spec.js
+++ e2e/test.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test1', async ({ page }) => {
+test('expect title to be displayed', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page).toHaveTitle(/Minesweeper/i);
 });
