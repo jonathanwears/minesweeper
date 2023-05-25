490ce3c59464e9c59df29046e29eb69222fc4ff6
Jonathan <jonathanwears@outlook.com>
2023-05-25 16:23:41 +0200

    reworked tests

--- tests/tile.test.jsx
+++ tests/tile.test.jsx
@@ -1,39 +1,19 @@
 import React from 'react';
-import { test, expect, describe, beforeAll, afterAll } from 'vitest';
-import { screen, render, fireEvent } from '@testing-library/react';
+import { test, expect, describe, beforeAll } from 'vitest';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Tile from '../src/components/Tile';
 import useTileStore from '../src/utils/stores/useTileStore';
 import useGameStore from '../src/utils/stores/useGameStore';
-import StatusBar from '../src/components/StatusBar';
+import StatusBar from '../src/pages/StatusBar';
+import tile from './mockTiles';
 
-describe.skip('tile', () => {
+describe('tile', () => {
+  const user = userEvent.setup();
   beforeAll(() => {
-    const arr = new Array(5);
     useTileStore.setState(
       {
-        tiles: [
-          {
-            isFlagged: false,
-            isMine: false,
-            isClicked: false,
-          },
-          {
-            isFlagged: false,
-            isMine: false,
-            isClicked: false,
-          },
-          {
-            isFlagged: false,
-            isMine: true,
-            isClicked: false,
-          },
-          ...arr,
-          {
-            isMine: true,
-            isClicked: false,
-            isFlagged: false,
-          },
-        ],
+        tiles: tile,
       },
     );
     useGameStore.setState(
@@ -45,6 +25,38 @@ describe.skip('tile', () => {
         },
       },
     );
+  });
+
+  test('tiles are rendered', () => {
+    render(
+      <>
+        <Tile index={0} />
+        <Tile index={1} />
+        <Tile index={2} />
+
+      </>,
+    );
+    const tile0 = screen.getByLabelText('listItem0');
+    const tile2 = screen.getByLabelText('listItem2');
+    expect(tile0).toBeTruthy();
+    expect(tile2).toBeTruthy();
+  });
+
+  test('a basic number tile displays a number when click', async () => {
+    render(
+      <>
+        <Tile index={0} />
+        <Tile index={1} />
+        <Tile index={2} />
+      </>,
+    );
+    const tile0 = screen.getByRole('listitem', { name: 'listItem0' });
+    await user.click(tile0);
+    const tileNumber = screen.getByText(/[1]/i);
+    expect(tileNumber).toBeInTheDocument();
+  });
+
+  test('lost message is displayed when a mine is clicked', async () => {
     render(
       <>
         <Tile index={0} />
@@ -53,30 +65,9 @@ describe.skip('tile', () => {
         <StatusBar />
       </>,
     );
-  });
-
-  test('if tiles exists in document', () => {
-    const tile0 = screen.getByLabelText('listItem0');
-    const tile2 = screen.getByLabelText('listItem1');
-    expect(tile0).toBeTruthy();
-    expect(tile2).toBeTruthy();
-  });
-
-  test('expect tile to exist', () => {
-    const tile0 = screen.getByLabelText('listItem0');
-    expect(tile0).toBeTruthy();
-  });
-
-  test('tile displays number when not mine and clicked', () => {
-    const tile0 = screen.getByLabelText('listItem0');
-    fireEvent.click(tile0);
-    expect(tile0.querySelector('p').innerHTML).toMatch(/[1]/);
-  });
-
-  test('status is changed to lost when clicking mine', () => {
-    const tile2 = screen.getByLabelText('listItem2');
-    fireEvent.click(tile2.querySelector('div'));
-    const statusMessage = screen.getByText(/aww you lost!/i).innerHTML;
-    expect(statusMessage).toMatch(/aww You Lost!/i);
+    const tile2 = screen.getByRole('listitem', { name: /listItem2/i });
+    await user.click(tile2);
+    const statusMessage = screen.getByText(/aww you lost!/i);
+    expect(statusMessage).toBeInTheDocument();
   });
 });
