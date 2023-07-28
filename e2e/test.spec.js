2836125d07670e8f1f9eea4d1526af4e4ddee2d0
Jonathan <jonathanwears@outlook.com>
2023-07-28 12:23:19 +0200

    added more tests

--- e2e/test.spec.js
+++ e2e/test.spec.js
@@ -1,6 +1,16 @@
+/* eslint-disable no-await-in-loop */
 import { test, expect } from '@playwright/test';
 
 test('expect title to be displayed', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page).toHaveTitle(/Minesweeper/i);
+});
+
+test('lose Game', async ({ page }) => {
+  await page.goto('http://localhost:3000/test');
+  await page.getByRole('button', { name: 'Start Game' }).click();
+
+  await page.getByRole('list').locator('button').locator('nth=0').click();
+  const a = await page.getByText('aww you Lost!');
+  await expect(a).toBeVisible();
 });
