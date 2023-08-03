a6960de021b2e53038ef8275837c3ffd13868ffd
Jonathan <jonathanwears@outlook.com>
2023-08-03 13:48:53 +0200

    added css

--- src/icons/FlagIcon.jsx
+++ src/icons/FlagIcon.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import flag from '../assets/flag.png';
 
 function FlagIcon() {
-  return <img className="flag" alt="flag" src={flag} />;
+  return (
+    <img
+      className="absolute w-1/3"
+      alt="flag"
+      src={flag}
+    />
+  );
 }
 
 export default FlagIcon;
