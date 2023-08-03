69ff9ef6b5ae5a90a38bc21039ed06c7270ff723
Jonathan <jonathanwears@outlook.com>
2023-08-03 14:14:56 +0200

    removed unneeded logic

--- src/utils/checkUiConditions.js
+++ src/utils/checkUiConditions.js
@@ -1,42 +1,19 @@
+/* eslint-disable consistent-return */
 import useTileStore from './stores/useTileStore';
 import useGameStore from './stores/useGameStore';
 import checkOnClick from './checkOnClick';
 
 function checkUiConditions(index) {
-  const { isMine, isClicked } = useTileStore.getState().tiles[index];
+  const { isClicked } = useTileStore.getState().tiles[index];
   const { isWon, isLost } = useGameStore.getState().game;
 
-  const conditions = {
-    reveal: false,
-    ui: '', // mine, flagged, clicked, default
-    tileMineNum: '',
-  };
-
-  if (isLost || isWon) {
-    return conditions;
+  if (isLost || isWon || !isClicked) {
+    return;
   }
 
-  if (!isClicked) {
-    if (conditions.ui !== 'default') {
-      conditions.ui = 'default';
-      conditions.flagged = null;
-    }
-    return conditions;
-  }
+  const tileNumber = checkOnClick(index);
 
-  if (isMine) {
-    conditions.ui = 'mine';
-    return conditions;
-  }
-
-  // default if no other conditions are met. isClicked should always be true.
-  if (isClicked) {
-    conditions.ui = 'clicked';
-    conditions.tileMineNum = checkOnClick(index);
-    return conditions;
-  }
-
-  return null;
+  return tileNumber;
 }
 
 export default checkUiConditions;
