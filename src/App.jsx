8c2fcc7a5aeff8297a8378e71849e16a06834c26
Jonathan <jonathanwears@outlook.com>
2023-08-04 15:41:41 +0200

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import StatusBar from './pages/StatusBar';
 import GameBoard from './pages/GameBoard';
 
 function App() {
   return (
-    <div className="bg-white w-full h-screen m-0 grid grid-rows-1 grid-cols-3  ">
+    <div className="bg-white w-full h-screen m-0 lg:grid lg:grid-rows-1 lg:grid-cols-3 md:flex md:flex-col ">
       <div className=" grid grid-rows-1">
         <StatusBar />
       </div>
-      <div className="grid-rows-2 col-span-2 justify-center items-center flex flex-col">
-        <GameBoard />
-      </div>
+      <GameBoard />
     </div>
   );
 }
