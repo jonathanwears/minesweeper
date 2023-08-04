558a3f0889128ece95d7e673f653c93e51343067
Jonathan <jonathanwears@outlook.com>
2023-08-04 16:09:17 +0200

    added conditional css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 import StatusBar from './pages/StatusBar';
 import GameBoard from './pages/GameBoard';
+import useGameStore from './utils/stores/useGameStore';
 
 function App() {
+  const { inProgress } = useGameStore((state) => state.game);
+  const className = classNames('bg-white w-full h-screen m-0 lg:grid  md:flex md:flex-col', {
+    'lg:grid-rows-1 lg:grid-cols-3': inProgress,
+  });
   return (
-    <div className="bg-white w-full h-screen m-0 lg:grid lg:grid-rows-1 lg:grid-cols-3 md:flex md:flex-col ">
+    <div className={className}>
       <div className=" grid grid-rows-1">
         <StatusBar />
       </div>
